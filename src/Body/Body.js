a742182891793fa7ef869c9313d72dc252ff8acd
Elnur <gamidovelnur@mail.ru>
2022-12-03 23:35:19 +0400

    created header and footer

---
+++ src/Body/Body.js
@@ -0,0 +1,10 @@
+
+
+export default function Body(){
+
+    return(
+        <div className="main">
+            
+        </div>
+    );
+}
---
+++ src/Header/Header.js
@@ -0,0 +1,19 @@
+
+
+export default function Header(){
+
+    return(
+        <div className="header">
+                <nav className="navbar navbar-light justify-content-left">
+                 
+                        <p style={{color: "#fff"}} className="navbar-brand header-title">We work for your comfort</p>
+                        <form className="form-inline header-form">
+                            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-warning my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+             
+                </nav>
+
+        </div>
+    );
+}
